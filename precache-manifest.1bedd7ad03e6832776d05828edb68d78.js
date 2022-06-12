0d7e55851c446cde7c6efe42f0cf13904d02c20e
mehdi_aq <douahmehdi.p@gmail.com>
2022-06-13 01:29:37 +0200

    redesign

---
+++ precache-manifest.1bedd7ad03e6832776d05828edb68d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdbef3df6a298bc5f484",
+    "url": "/static/css/main.7a915147.chunk.css"
+  },
+  {
+    "revision": "cdbef3df6a298bc5f484",
+    "url": "/static/js/main.cdbef3df.chunk.js"
+  },
+  {
+    "revision": "cfda1b899bd53eb4481d",
+    "url": "/static/js/1.cfda1b89.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce00cc68206cf488dc4996257afcfb5c",
+    "url": "/index.html"
+  }
+];
